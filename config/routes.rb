7d8b32c51ce269a4679cf6491091a7f731140119
Derien Stephens <derienstephens@Deriens-MacBook-Pro.local>
2020-04-28 18:28:34 -0400

    adding devise users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :chats
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'chats#index' 
+  resources :chat_rooms, only: [:new, :create, :show, :index]
+
+  root 'chat_rooms#index'
 
 end
